1a93664da338b37cefb5eed73361fcdd2c717567
ooopiz <r.huang850@gmail.com>
2024-10-02 12:03:51 +0800

    Fix

--- action_test.py
+++ action_test.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 import os
+import requests
 
-print('action test')
+
 for key, value in os.environ.items():
     print(f'{key}: {value}')
+
+token = os.environ.get('SUPER_VAR')
+
+
+token_list = [token]
+for token in token_list:
+    headers = {"Authorization": "Bearer " + token}
+    payload = {'message': 'hello'}
+    url = 'https://notify-api.line.me/api/notify'
+    response = requests.post(url, headers=headers, data=payload)
