5ef2df20509651281824df7583c4e73539f230d6
ooopiz <r.huang850@gmail.com>
2024-10-02 12:10:16 +0800

    Fix

--- action_test.py
+++ action_test.py
@@ -7,12 +7,10 @@ import requests
 for key, value in os.environ.items():
     print(f'{key}: {value}')
 
-token = os.environ.get('SUPER_VAR')
-
-
-token_list = [token]
+token_env = os.environ.get('SUPER_VAR')
+token_list = token_env.split(';')
 for token in token_list:
     headers = {"Authorization": "Bearer " + token}
-    payload = {'message': 'hello'}
+    payload = {'message': 'hello2'}
     url = 'https://notify-api.line.me/api/notify'
     response = requests.post(url, headers=headers, data=payload)
