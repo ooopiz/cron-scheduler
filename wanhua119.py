8190ba6970710a6529e99df35a022452061e1ac4
ooopiz <r.huang850@gmail.com>
2024-10-02 19:33:12 +0800

    Fix

--- wanhua119.py
+++ wanhua119.py
@@ -13,23 +13,25 @@ def main():
 
     taipei_timezone = pytz.timezone('Asia/Taipei')
     now = datetime.now(taipei_timezone)
-    five_minutes_ago = now - timedelta(minutes=5)
+    time_shift = now - timedelta(minutes=120)
 
     url = "https://service119.tfd.gov.tw/service119/citizenCase/caseList"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.post(url, headers=headers)
     data = response.json()
     wanhua_events = list(filter(
-        lambda x: '萬華' in x['csPlaceFuzzy'] and datetime.strptime(x['inTime'], "%Y/%m/%d %H:%M:%S").replace(tzinfo=taipei_timezone) > five_minutes_ago,
+        lambda x: '萬華區' in x['csPlaceFuzzy'] and datetime.strptime(x['inTime'], "%Y/%m/%d %H:%M:%S").replace(tzinfo=taipei_timezone) > time_shift,
         data['rows']))
 
     if len(wanhua_events) == 0:
-        print('No events in 5 minutes')
+        print('No events in 30 minutes')
         return
 
     message = "\nhttps://service119.tfd.gov.tw/service119/accCase"
     for case in wanhua_events:
-        message += f"\n{case['inTime']}, {case['csKindName']}, {case['csPlaceFuzzy']}, {case['caseStatus']}"
+        message += f"\n{case['inTime']} {case['csPlaceFuzzy']}, {case['csKindName']} {case['caseStatus']}"
+
+    print(message)
 
     token_list = env_token.split(';')
     for token in token_list:
